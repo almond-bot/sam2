34e0349a1a7cb2d10d1ddc03cece89026d8bd091
Almond Lambda <lambda@almondbot.com>
2025-05-17 22:25:50 +0000

    update paths

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 sam2 = build_sam2(
     "configs/sam2.1/sam2.1_hiera_l.yaml",
-    "/home/almond/meta/sam2/checkpoints/sam2.1_hiera_large.pt",
+    "checkpoints/sam2.1_hiera_large.pt",
     device=torch.device("cuda"),
     apply_postprocessing=False,
 )
